d4caa906f14a73c3a15e95c9f2c535586b4290b7
Alex <carlson3.alex@gmail.com>
2020-10-19 15:21:34 -0400

    async programming

--- server/models/users.js
+++ server/models/users.js
@@ -3,7 +3,10 @@
 
 const data = [{name: 'Alex', age: 29}, {name: 'Chris', age: 34}];
 
-function getAll(){
+async function getAll(){
+    //throw {status: 501, message: "This is a fake error"}
+    //await Promise.resolve()
+    console.log("Called Get All")
     return data;
 }
 
@@ -11,4 +14,4 @@ function add(name, age){
     data.push({name, age});
 }
 
-module.exports = { getAll, add, search: q=> data.filter(x=> x.name == q) }
+module.exports = { getAll, add, search: async q=> data.filter(x=> x.name == q) }
