92bf3ebe8a28539115593f624f3b93a01069f6fe
Alex <carlson3.alex@gmail.com>
2020-11-22 19:34:43 -0500

    added the catch all

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 require('dotenv').config();
+const path = require('path');
+
 const users = require('./controllers/users');
 const workouts = require('./controllers/workouts');
 const comments = require('./controllers/comments');
@@ -10,8 +12,12 @@ const port = process.env.PORT || 3000;
 
 console.log(process.env.BEST_CLASS);
 
+//  Middleware
 app.use(express.json());
+app.use(express.static(__dirname + '/../docs/'));
 
+
+//  Authentication
 app.use(function(req, res, next) {
   const arr = (req.headers.authorization || "").split(" ");
   if(arr.length > 1 && arr[1] != null){
@@ -20,6 +26,8 @@ app.use(function(req, res, next) {
   next();
 });
 
+
+//  API
 app.get('/', (req, res, next) => {
   res.send('Hello Hudson Valley! You requested ' + req.url)
 })
@@ -28,6 +36,14 @@ app.use('/workouts', workouts);
 app.use('/comments', comments);
 app.use('/reactions', reactions);
 app.use('/followers', followers);
+
+app.get('*', (req, res, next) => {
+  const filename = path.join(__dirname, '/../docs/index.html');
+  console.log(filename);
+  res.sendFile( filename );
+})
+
+
 app.use( (err, req, res, next) =>{
     console.log(err);
     res.status(err.status || 500).send( { message: err.message } )
