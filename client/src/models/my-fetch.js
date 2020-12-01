e4ec0394b55cba468c68d477f84c5feaacf6f3be
Alex <carlson3.alex@gmail.com>
2020-12-01 00:58:07 -0500

    new docs

--- client/src/models/my-fetch.js
+++ client/src/models/my-fetch.js
@@ -1,7 +1,7 @@
 
 /*
 */
-const API_ROOT = process.env.API_ROOT || 'http://localhost:3001/';
+const API_ROOT = process.env.API_ROOT || '/';
 
 export function myFetch(url, data = null, method = null){
 
